53c3657e80e05c405519e733d0689910304f3ee7
Ali Raza <aliraza021419@gmail.com>
2023-04-05 19:04:47 -0400

    Added buttons for the numbers and the operators and displays number on screen when clicking the associated number button

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // variables for calculator operation //
 let num1, num2, operator;
 
+// populate the display of the calculator when a number button is clicked //
+const output = document.querySelector('.output');
+const numbers = document.querySelector('.numbers');
+
+// numbers.onclick = (e) => {
+//     if (e.target.tagName === 'BUTTON') {
+//         output.textContent = e.target.innerText;
+//     }
+// }
+numbers.onclick = (e) => e.target.tagName === 'BUTTON' ? output.textContent = e.target.innerText: null;
+
 // function that takes 2 numbers and an operator and returns the correct operation //
 function operate(num1, num2, operator) {
     if (operator == "+") {
