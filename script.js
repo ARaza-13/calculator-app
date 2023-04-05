1ce925dce01b70a625b939024488de8902b97247
Ali Raza <aliraza021419@gmail.com>
2023-04-05 18:12:59 -0400

    Created a function that takes in 2 numbers and an operator which returns the correct operation

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
-// a function that takes in multiple values and returns the sum //
+// variables for calculator operation //
+let num1, num2, operator;
+
+// function that takes 2 numbers and an operator and returns the correct operation //
+function operate(num1, num2, operator) {
+    if (operator == "+") {
+        return add(num1, num2);
+    } else if (operator == "-") {
+        return subtract(num1, num2);
+    } else if (operator == "*") {
+        return multiply(num1, num2);
+    } else if (operator == "/") {
+        return divide(num1, num2);
+    } else {
+        return "Invalid Operation";
+    }
+}
+
+// functions that takes in multiple values and returns the respective sum, difference, product, and quotient //
 function add(...args) {
     return args.reduce((total, current) => {
         return total + current;
     });
 }
 
-// a function that takes in multiple values and returns the difference //
 function subtract(...args) {
     return args.reduce((total, current) => {
         return total - current;
     });
 }
 
-// a function that takes in multiple values and returns the product //
 function multiply(...args) {
     return args.reduce((total, current) => {
         return total * current;
     });
 }
 
-// a function that takes in multiple values and returns the quotient //
 function divide(...args) {
     return args.reduce((total, current) => {
         return total / current;
