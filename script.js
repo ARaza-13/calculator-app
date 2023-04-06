003f591264a69177564d7cb56fe75d7cdee7857e
Ali Raza <aliraza021419@gmail.com>
2023-04-06 18:49:38 -0400

    Able to perform a single operation from 2 selected numbers and an operator (need to figure out how to string together several operations and evaluate more than a single pair of numbers at a time)

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 // variables for calculator operation //
-let num1, num2, operator;
+let num1 = "";
+let num2 = "";
+let operator = "";
+
+// boolean variable to check if an operator has been clicked, if so we stop updating the number //
+let operatorSelected = false; 
 
 // populate the display of the calculator when a number button is clicked //
 const output = document.querySelector('.output');
 const numbers = document.querySelector('.numbers');
+const operators = document.querySelector('.operators');
+const equals = document.querySelector('.equals');
 
-// numbers.onclick = (e) => {
-//     if (e.target.tagName === 'BUTTON') {
-//         output.textContent = e.target.innerText;
-//     }
-// }
-numbers.onclick = (e) => e.target.tagName === 'BUTTON' ? output.textContent = e.target.innerText: null;
+numbers.onclick = (e) => getNums(e.target);
+operators.onclick = (e) => getOperator(e.target);
+equals.onclick = () => output.textContent = operate(+num1, +num2, operator);
+
+// once we select an operator, we finish storing the first number and then begin selecting the second number //
+function getNums(evt) {
+    if (!operatorSelected) {
+        num1 += evt.innerText;
+        output.textContent = num1;
+    } else {
+        num2 += evt.innerText;
+        output.textContent = num2;
+    }
+    return [num1, num2];
+}
+
+function getOperator(evt) {
+    operatorSelected = true;
+    operator = evt.innerText;
+    return operator;
+}
 
 // function that takes 2 numbers and an operator and returns the correct operation //
 function operate(num1, num2, operator) {
@@ -18,7 +40,7 @@ function operate(num1, num2, operator) {
         return add(num1, num2);
     } else if (operator == "-") {
         return subtract(num1, num2);
-    } else if (operator == "*") {
+    } else if (operator == "x") {
         return multiply(num1, num2);
     } else if (operator == "/") {
         return divide(num1, num2);
